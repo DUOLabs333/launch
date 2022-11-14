a6c8c174e21bbfd285be8a14b83d224a06eab3ac
DUOLabs333 <dvdugo333@gmail.com>
2022-11-14 09:19:09 -0500

    Add support for MacOS

--- src/launch/__init__.py
+++ src/launch/__init__.py
@@ -5,6 +5,26 @@ import subprocess,re
 desktop_file_paths=[os.path.expanduser("~")+"/.local/share/applications","/usr/share/applications","/usr/local/share/applications"]
 applications={}
 def extractApplications():
+    if sys.platform=='darwin':
+        stack=list(os.scandir('/Applications'))
+        visited={}
+        while len(stack)>0:
+            v=stack.pop()
+            if v not in visited:
+                #Visit
+                visited[v]=True
+                if v.name.endswith(".app"):
+                    info=[]
+                    info.append(' '.join(['open','-a',f'"{v.path}"']))
+                    info.append(False)
+                    applications[v.name[:-4]]=info
+                    continue #No need to search deeper
+                if not v.is_dir():
+                    continue
+                for w in os.scandir(v):
+                    if w not in visited:
+                        stack.append(w)  
+        return      
     for path in desktop_file_paths:
         for file in os.listdir(path):
             if not file.endswith(".desktop"):
@@ -18,7 +38,7 @@ def extractApplications():
     
 
 def matchApplications():
-    fzf = subprocess.Popen(["fzf","-1","-0","--prompt=Application: ","--query="+(sys.argv[1] if len(sys.argv)>1 else "")], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    fzf = subprocess.Popen(["fzf","-1","-e","-0","--prompt=Application: ","--query="+(sys.argv[1] if len(sys.argv)>1 else "")], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
     selection=fzf.communicate(input='\n'.join(applications.keys()).encode())[0].decode().strip()
     if (fzf.returncode==1):
         print("No matching applications!")
